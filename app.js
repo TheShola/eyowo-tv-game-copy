e18145cb96b9cf04f6574d5264ae38c06fb47764
Dimeji Abidoye <dimejy2@gmail.com>
2019-01-13 00:31:25 +0100

    improved 404 support

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) =>{
 });
 
 
-app.get('/next', (req, res) =>{ 
+app.get('/next', (req, res) =>{
     EventHandler.broadcastNextQuestion();
     res.end('<p> a question has been broadcast <a href="/next">click to broadcast the next question</a> </p>');
 });
@@ -31,7 +31,19 @@ app.get('/reveal', (req, res)=> {
 });
 
 app.get('*', function(req, res){
-  res.send('Wild monkeys have stolen this route. Try another!', 404);
+    res.status(404).send(`
+    <html>
+    <head><meta name="viewport" content="width=device-width, minimum-scale=0.1"></head>
+    <body>
+    <div stlye ="margin: auto; width: 50%; border: 3px solid green; padding: 10px;">
+    <marquee behavior="alternate" direction="left">Oh no! a monkey has stolen this route</marquee>
+    <marquee behavior ="alternate" direction="right"  scrollamount="20">
+    <img style="display: block; margin-left: auto; margin-right: auto; height:70%;" src="https://i.imgur.com/1HPuAa2.gif">
+    </marquee>
+    </div>
+    </body>
+    </html>
+    `);
 });
 
 
