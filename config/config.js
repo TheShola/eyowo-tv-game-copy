0920f37844e1d8d19e8e07ca9ab3bc88ca18d0a5
Shola Abimbola <adesolaabimbola@gmail.com>
2019-03-03 11:57:45 +0100

    first commit

--- config/config.js
+++ config/config.js
@@ -1,2 +1,2 @@
-module.exports.redisUrl = "127.0.0.1:6379";
+module.exports.redisUrl = "redis://chidi:LhJoEY6KLT3xeEhoPocFFZ64THpjpJfe@redis-18766.c8.us-east-1-4.ec2.cloud.redislabs.com:18766";
 module.exports.port = process.env.PORT || 3456;
